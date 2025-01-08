a4e5a763e4512d43ce9efa85c35476197240bbfd
Boden Harris <blharris@umass.edu>
2025-01-08 18:43:21 -0500

    Resume Added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 
-<div class="min-h-screen bg-gray-800 p-6">
-    <h1 class="text-5xl font-extrabold text-white">Boden Harris</h1>
+<div class="min-h-screen bg-gray-800 p-8">
+    <h1 class="text-6xl font-extrabold text-white">Boden Harris</h1>
     <h2 class="text-lg text-gray-300 mt-2">blharris@umass.edu</h2>
+    <a href="Boden Harris - Resume.pdf" class="text-blue-400 mt-4 inline-block hover:text-blue-600">
+        View My Resume
+    </a>
 </div>
