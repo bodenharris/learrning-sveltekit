447d79fa5e615481a2c41544355dc50b54fd6a4b
Boden Harris <blharris@umass.edu>
2025-01-08 18:10:47 -0500

    Formatting 1.1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 
-<div class="min-h-screen bg-gray-800 flex flex-col items-center justify-center text-center p-6">
+<div class="min-h-screen bg-gray-800 p-6">
     <h1 class="text-5xl font-extrabold text-white">Boden Harris</h1>
     <h2 class="text-lg text-gray-300 mt-2">blharris@umass.edu</h2>
 </div>
