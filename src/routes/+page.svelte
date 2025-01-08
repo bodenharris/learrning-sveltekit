697d7b0ad0de079b31f47f016d6fe124dac8c87e
Boden Harris <blharris@umass.edu>
2025-01-08 16:46:04 -0500

    Updated box size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<div class="w-64 h-64 bg-red-500"></div>
+<div class="w-64 h-128 bg-red-500"></div>
