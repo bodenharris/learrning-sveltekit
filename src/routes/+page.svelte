0a8b667a1660f0030e37dcba9d33f42bee0a9178
Boden Harris <blharris@umass.edu>
2025-01-27 14:58:24 -0500

    added divider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 
 <div class="min-h-screen bg-gray-800 p-8">
     <h1 class="text-6xl font-extrabold text-white">Boden Harris</h1>
+    <hr class="border-t-4 border-gray-500">
     <h2 class="text-lg text-gray-300 mt-2">blharris@umass.edu</h2>
     <a href="Boden Harris - Resume.pdf" class="text-blue-400 mt-4 inline-block hover:text-blue-600">
         View My Resume
