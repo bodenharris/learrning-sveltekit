1098516b536651d6c844e565473cc662c6148f43
Boden Harris <blharris@umass.edu>
2025-01-27 15:05:18 -0500

    reformat paragraphs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,11 @@
     <h1 class="text-6xl font-extrabold text-white">Boden Harris</h1>
     <hr class="border-t-2 border-gray-500">
     <h2 class="text-lg text-gray-300 mt-2">blharris@umass.edu</h2>
-    <a href="Boden Harris - Resume.pdf" class="text-blue-400 mt-4 inline-block hover:text-blue-600">
-        View My Resume
-    </a>
     <p class="text-gray-300 mt-4 border border-gray-500 rounded-lg w-1/2 h-auto p-3" >
         <b class="font-bold text-2xl">About me: </b><br>
         I am a freshman at the UMass Amerst Commonwealth Honors College studying Computer science. I am currently on track to graduate in the winter of 2026. 
     </p>
+    <a href="Boden Harris - Resume.pdf" class="text-blue-400 mt-4 inline-block hover:text-blue-600">
+        View My Resume
+    </a>
 </div>
