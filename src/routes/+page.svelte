0f69fb02024cee526ccd8b980b07bd373f7c80ff
Boden Harris <blharris@umass.edu>
2025-01-28 12:05:45 -0500

    nightmode edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 
-<div class="min-h-screen {bgColor} p-8">
+<div class="min-h-screen {bgColor} p-8 text-{textColor}">
     <h1 class="text-6xl font-extrabold text-white">Boden Harris</h1>
     <button 
-    class="absolute w-16 h-16 top-4 right-4 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+    class="absolute w-16 h-16 top-4 right-4 flex items-center justify-center rounded-full {bgColor} hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
     aria-label="Toggle Night Mode"
     on:click={toggleNightMode}>
         <img src="moonClipart.png" alt="Moon Icon" class="w-[48px] h-[48px]">
     </button>
-    <hr class="border-t-2 border-gray-500">
-    <h2 class="text-lg text-gray-300 mt-2">blharris@umass.edu</h2>
-    <p class="text-gray-300 mt-4 border border-gray-500 rounded-lg w-1/2 h-auto p-3" >
+    <hr class="border-t-2 {borderColor}">
+    <h2 class="text-lg ">blharris@umass.edu</h2>
+    <p class=" mt-4 border {borderColor} rounded-lg w-1/2 h-auto p-3" >
         <b class="font-bold text-2xl">About me: </b><br>
         I am a freshman at the UMass Amerst Commonwealth Honors College studying Computer science. I am currently on track to graduate in the winter of 2026. 
     </p>
@@ -26,7 +26,7 @@
         'text-gray-800', 'white'
     ];
     let borderColors = [
-        'border-gray-800', 'border-gray-300'
+        'border-gray-800', 'border-gray-500'
     ];
     let bgColor = bgColors[1];
     let textColor = textColors[1];
@@ -35,9 +35,13 @@
     function toggleNightMode() {
 
        if (nightMode) {
-        bgColor = bgColors[0]; 
+        bgColor = bgColors[0];
+        textColor = textColors[0];
+        borderColor = borderColors[0]; 
        } else {
         bgColor = bgColors[1];
+        textColor = textColors[1];
+        borderColor = borderColors[1]; 
        }
        nightMode=!nightMode;
     }
