c1e4abaaecf79dd828449616b902b35c897ed724
Boden Harris <blharris@umass.edu>
2025-02-03 16:16:05 -0500

    added ring around pfp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
         <b class="font-bold text-2xl">About me: </b><br>
         I am a freshman at the UMass Amerst Commonwealth Honors College studying Computer science.<br>I am currently on track to graduate in the winter of 2026. 
     </p>
-    <button class = "absolute right-2 top-25 rounded-full h-96 w-96 hover:ring-2 ring-grey-500 transition">    
+    <button class = "absolute right-2 top-25 rounded-full h-96 w-96 ring-4 ring-grey-500 transition">    
         <img src="profile.jpg" alt="" class="h-96 w-96 rounded-full">
     </button>
     <p class="mt-4 border {borderColor} rounded-lg w-1/2 h-auto p-1 flex items-center space-x-2">
